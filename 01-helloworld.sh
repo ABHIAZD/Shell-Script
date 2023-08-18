3a6202379f563c058edf5ab151e982bcbe93ea54
ABHIAZD <abhishekkarnekanti@gmail.com>
2023-08-18 23:11:02 +0530

    modifiesd

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "hi hello world"
-echo "Hi abhishek"
-echo "Hi mahesh"
-echo "how are you abhishek"
-echo "fine mahesh"
+p1=abhishek
+p2=mahesh
+echo "Hi $p1"
+echo "Hi $p2"
+echo "how are you $p2"
+echo "fine $p1"
